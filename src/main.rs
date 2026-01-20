1852aeccd21003d13202379f64f43754c741785e
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2026-01-20 01:33:02 -0700

    Add dotenv support and configure disk location from environment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use dotenvy::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let disk_location = env::var("DISK_LOCATION").unwrap_or(String::from("default_disk.qcow2"));
+    println!("Disk location: {:?}", disk_location);
 }
