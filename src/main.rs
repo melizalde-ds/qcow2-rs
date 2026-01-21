5d30bcf5d2d26f1c5dae4327ff3c9e1c5450d48a
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2026-01-21 09:41:00 -0700

    Refactor main function to streamline QCOW2 header loading and parsing

--- src/main.rs
+++ src/main.rs
@@ -38,31 +38,25 @@ fn main() {
     // Retrieve the disk location from environment variables
     let disk_location = get_disk_location();
 
-    // Load the QCOW2 disk image
-    let qcow2_image = load_qcow2_image(&disk_location);
-    print!("Loaded QCOW2 image of size: {} bytes\n", qcow2_image.len());
+    // Create Header Buffer
+    let mut initial_buff: [u8; 72] = [0u8; 72];
+
+    // Load the first 72 bytes of the QCOW2 file
+    let mut file = File::open(&disk_location).expect("Failed to open QCOW2 file");
+    file.read_exact(&mut initial_buff)
+        .expect("Failed to read QCOW2 header");
 
     // Parse and display QCOW2 header information
-    let version = qcow2_image[7];
+    let version = initial_buff[7];
     print!("QCOW2 version: {}\n", version);
 
-    let header_length: u32;
     if version == 3 {
-        header_length = u32::from_be_bytes(qcow2_image[100..104].try_into().unwrap());
-    } else {
-        header_length = 72;
+        let mut v3_buff: [u8; 104] = [0u8; 104];
+        file.read_exact(&mut v3_buff)
+            .expect("Failed to read QCOW2 v3 header");
     }
-    print!("QCOW2 header length: {} bytes\n", header_length);
 }
 
 fn get_disk_location() -> String {
     env::var("DISK_LOCATION").unwrap_or(String::from("default_disk.qcow2"))
-}
-
-fn load_qcow2_image(path: &str) -> Vec<u8> {
-    let mut file = File::open(path).expect("Failed to open QCOW2 file");
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)
-        .expect("Failed to read QCOW2 file");
-    buffer
 }
