e2fba5848b9795d8fbd2d1597fb49067bd1f0b57
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2026-01-21 09:32:04 -0700

    Add QCOW2 header and metadata structures; enhance header parsing logic

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,34 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
+struct Qcow2Header {
+    magic: u32,
+    version: u32,
+    backing_file_offset: u64,
+    backing_file_size: u32,
+    cluster_bits: u32,
+    size: u64,
+    crypt_method: u32,
+    l1_size: u32,
+    l1_table_offset: u64,
+    refcount_table_offset: u64,
+    refcount_table_clusters: u32,
+    nb_snapshots: u32,
+    snapshots_offset: u64,
+}
+
+struct Qcow2V3Header {
+    incompatible_features: u64,
+    compatible_features: u64,
+    autoclear_features: u64,
+    refcount_order: u32,
+    header_length: u32,
+}
+struct Qcow2Metadata {
+    header: Qcow2Header,
+    v3_header: Option<Qcow2V3Header>,
+}
+
 fn main() {
     // Load environment variables from .env file
     dotenv().ok();
@@ -14,14 +42,17 @@ fn main() {
     let qcow2_image = load_qcow2_image(&disk_location);
     print!("Loaded QCOW2 image of size: {} bytes\n", qcow2_image.len());
 
-    // Read and prints the header information (first 72 bytes for QCOW2 or at least 104 bytes for QCOW2 v3)
+    // Parse and display QCOW2 header information
     let version = qcow2_image[7];
-    let header_size = qcow2_image[103] as usize;
-    let header = &qcow2_image[..header_size];
+    print!("QCOW2 version: {}\n", version);
 
-    print!("QCOW2 Version: {}\n", version);
-    println!("QCOW2 Header Size: {} bytes", header_size);
-    print!("QCOW2 Header: {:x?}\n", header);
+    let header_length: u32;
+    if version == 3 {
+        header_length = u32::from_be_bytes(qcow2_image[100..104].try_into().unwrap());
+    } else {
+        header_length = 72;
+    }
+    print!("QCOW2 header length: {} bytes\n", header_length);
 }
 
 fn get_disk_location() -> String {
